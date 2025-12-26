0a5613dd38001c136431a855de53422bf2c89d71
Ru Chern Chong <hello@ruchern.dev>
2025-12-27 03:24:33 +0800

    feat(web): add analytics tracking

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import Script from "next/dist/client/script";
+import type { ReactNode } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +24,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -29,7 +32,14 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
+      <Script
+        defer
+        src="https://umami.tartinerlabs.com/script.js"
+        data-website-id="f76430ee-22b4-49a4-ad62-7ac393f12116"
+        data-domains="releasewatch.dev"
+      />
     </html>
   );
 }
