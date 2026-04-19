ca9e1eade00464af79b4dd1b1b44612ac381bf97
Ru Chern Chong <hello@ruchern.dev>
2026-04-19 21:40:36 +0800

    chore: migrate commitlint config to TypeScript

---
+++ commitlint.config.ts
@@ -0,0 +1,32 @@
+import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
+
+const config: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      RuleConfigSeverity.Error,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "scope-enum": [
+      RuleConfigSeverity.Error,
+      "always",
+      ["api", "web", "database", "types"],
+    ],
+  },
+};
+
+export default config;
